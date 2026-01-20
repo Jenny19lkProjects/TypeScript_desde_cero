4c71cb4cd6ab366e776020345ad7e707eb1ce263
Jenny19lkProyectos <jenny.19lk.projects@gmail.com>
2026-01-20 20:08:37 +0100

    Arrays

---
+++ Bases/tipos/arrays.ts
@@ -0,0 +1,9 @@
+(()=>{
+    const numbers: number[] =[1,2,3,4,5,6,7,8,9,10];
+    numbers.push(11)
+    console.log(numbers)
+
+    const villian=['Omega Rojo', 'Doramamu','Duende Verde']
+
+    villian.forEach(v=>console.log(v.toUpperCase()))
+})()
