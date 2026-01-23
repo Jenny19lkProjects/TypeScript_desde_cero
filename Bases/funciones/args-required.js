ad1cb0806b515bcc1e9d625753313345b1086707
Jenny19lkProyectos <jenny.19lk.projects@gmail.com>
2026-01-23 14:48:50 +0100

    párametros obligatorio de las funciones

---
+++ Bases/funciones/args-required.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+(() => {
+    const fullName = (firstName, lastName) => {
+        return `${firstName} ${lastName}`;
+    };
+    const name = fullName('Tony', 'Stark');
+    console.log({ name });
+})();
+//# sourceMappingURL=args-required.js.map
