17de4774083d53246a22f9ced2a2f5e9aedb375d
Jenny19lkProyectos <jenny.19lk.projects@gmail.com>
2026-02-22 19:20:22 +0100

    basica.ts

---
+++ Bases/classes/basica.ts
@@ -0,0 +1,11 @@
+(()=>{
+ class Avenger{
+    private name:string;
+    public team:string;
+    public realName:string;
+    
+ }
+ const antman:Avenger=new Avenger();
+ console.log(antman);
+
+})()
