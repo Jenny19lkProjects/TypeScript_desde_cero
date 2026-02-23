a80fbe014b1f27fb8611e9c8c303f50b3a8f580d
Jenny19lkProyectos <jenny.19lk.projects@gmail.com>
2026-02-23 19:34:47 +0100

    MÉTODOS PÚBLICOS Y PRIVADOS

--- Bases/classes/basica.js
+++ Bases/classes/basica.js
@@ -1,19 +1,26 @@
 "use strict";
-
+Object.defineProperty(exports, "__esModule", { value: true });
 (() => {
     class Avenger {
         name;
         team;
         realName;
         static avgAge = 35;
+        static getAvgAge() {
+            return this.name;
+        }
         constructor(name, team, realName) {
             this.name = name;
             this.team = team;
             this.realName = realName;
         }
+        bio() {
+            return `${this.name}(${this.team})`;
+        }
     }
     const antman = new Avenger('Antman', 'Scott Lang', 'Capitan');
     console.log(antman);
-    console.log(Avenger.avgAge);
+    console.log(antman.bio());
+    console.log(Avenger.getAvgAge());
 })();
 //# sourceMappingURL=basica.js.map
