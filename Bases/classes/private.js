2d1276fc630fcb6d45cf6e5f91d44698faafbfe2
Jenny19lkProyectos <jenny.19lk.projects@gmail.com>
2026-02-24 18:52:54 +0100

    Constructor privado

---
+++ Bases/classes/private.js
@@ -0,0 +1,24 @@
+"use strict";
+
+(() => {
+    class Apocalipsis {
+        name;
+        static intance;
+        constructor(name) {
+            this.name = name;
+        }
+        static callApocalipsis() {
+            if (!Apocalipsis.intance) {
+                Apocalipsis.intance = new Apocalipsis('Soy apocalipsis el único');
+            }
+            return Apocalipsis.intance;
+        }
+        changeName(newName) {
+            this.name = newName;
+        }
+    }
+    const apocalipsis = Apocalipsis.callApocalipsis();
+    apocalipsis.changeName('Xavier');
+    console.log(apocalipsis);
+})();
+//# sourceMappingURL=private.js.map
