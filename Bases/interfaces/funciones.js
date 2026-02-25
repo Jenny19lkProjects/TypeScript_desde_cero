8321766a143987084d67e02b335e1ec43a9e2547
Jenny19lkProyectos <jenny.19lk.projects@gmail.com>
2026-02-25 21:32:36 +0100

    Funciones en interfaces

---
+++ Bases/interfaces/funciones.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+(() => {
+    let addNumbersFunction;
+    addNumbersFunction = (a, b, c) => {
+        return 10;
+    };
+})();
+//# sourceMappingURL=funciones.js.map
