7cf5f31d77bc3551d6612d1b331dc7eacc32c28d
Jenny19lkProyectos <jenny.19lk.projects@gmail.com>
2026-03-05 15:14:43 +0100

    fin

---
+++ ts-node/src/index.ts
@@ -0,0 +1,7 @@
+import { Pokemon } from "./decorators/pokemon-class";
+
+const charmander=new Pokemon('Charmander');
+//(Pokemon.prototype as any).customName='Pikachu'
+//console.log(charmander.savePokemonToDB(50));
+charmander.publicApi='https://fernando-herrera.com';
+console.log(charmander)
--- ts-node/src/interfaces/index.ts
+++ ts-node/src/interfaces/index.ts
@@ -1,3 +1,4 @@
 export  { Hero } from "./hero";
 export { Villian } from './villain';
+import { Pokemon } from './pokemon';
 
